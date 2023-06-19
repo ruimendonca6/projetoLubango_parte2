826f0fe2f1b340c6c9cd7e2f443a99436e88756c
ruimendonca6 <ruiagmendonca@gmail.com>
2023-06-19 22:43:53 +0100

    navbar test 2

--- aplicacao/compunents/navbar.php
+++ aplicacao/compunents/navbar.php
@@ -1,13 +1,13 @@
-<nav>
-                <a href="./index.php"><img class="logo" src="/recursos/img/logo_angola2.png" /></a>
-                <div class="nav-links" id="navLinks">
-                    <i class="fa fa-times" onclick="hideMenu()"></i>
-                    <ul>
-                        <li><a href="/index.php">HOME</a></li>
-                        <li><a href="/src/Aplicacao/locais.php">LOCAIS TURÍSTICOS</a></li>
-                        <li><a href="/src/Aplicacao/alojamento.php">ALOJAMENTOS E RESTAURANTES</a></li>
-                        <li><a href="/src/Aplicacao/blog.php">BLOG</a></li>
-                    </ul>
-                </div>
-                <i class="fa fa-bars" onclick="showMenu()"></i>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/aplicacao/index.php">CIDADE DO LUBANGO</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="/aplicacao/index.php">HOME</a></li>
+      <li><a href="/aplicacao/locais.php">LOCAIS TURÍSTICOS</a></li>
+      <li><a href="/aplicacao/alojamento.php">ALOJAMENTO E RESTAURANTES</a></li>
+      <li><a href="/aplicacao/blog.php">BLOG</a></li>
+    </ul>
+  </div>
 </nav>
